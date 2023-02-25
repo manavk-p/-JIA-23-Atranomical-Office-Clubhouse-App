2ac640a04c259f3c181dd7e8bdbc832f4f90c419
manavk-p <manavkania@gmail.com>
2023-02-24 20:50:42 -0500

    add mock data for rooms screen, including desks, noise level, temperature

--- client/client-app/Rooms.js
+++ client/client-app/Rooms.js
@@ -6,6 +6,23 @@ import {
   SafeAreaView,
 } from "react-native";
 
+const mockData = {
+    rooms: [
+        {
+            noiseLevel: 'low',
+            occupancyLimit: 10,
+            temperature: 65,
+            numDesks: 10,
+        },
+        {
+            noiseLevel: 'high',
+            occupancyLimit: 15,
+            temperature: 68,
+            numDesks: 20,
+        }
+    ]
+}
+
 const Rooms = ({}) => {
   return (
     <SafeAreaView>
