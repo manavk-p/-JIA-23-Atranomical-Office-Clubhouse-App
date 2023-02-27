9d972059341075bb675fbb8bac6af92af56a238f
ahalwan3 <ahalwan3@gatech.edu>
2023-02-27 11:06:02 -0500

    [Few bug fixes including duplicate import, create event button not showing color]

--- client/client-app/App.js
+++ client/client-app/App.js
@@ -2,7 +2,6 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import AddEvent from './AddEvent';
 import EditEvent from './EditEvent';
-import AddEvent from './AddEvent';
 import Login from "./Login";
 import TabRoot from "./TabRoot";
 
@@ -21,7 +20,6 @@ function App() {
         <Stack.Screen name="TabRoot" component={TabRoot} />
         <Stack.Screen name="AddEvent" component={AddEvent} />
         <Stack.Screen name="EditEvent" component={EditEvent} />
-        <Stack.Screen name="AddEvent" component={AddEvent} />
 
       </Stack.Navigator>
     </NavigationContainer>
