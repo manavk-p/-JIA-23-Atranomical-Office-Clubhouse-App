f7fec0b92e9f80fe4b18c7b349b3e9a0751b021c
garrepi <jacgarrett18@gmail.com>
2023-04-13 15:25:06 -0400

    remove Home.js

--- client/client-app/TabRoot.js
+++ client/client-app/TabRoot.js
@@ -2,7 +2,6 @@ import React from "react";
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 
-import Home from "./Home";
 import Events from "./Events";
 //import Map from "./Map";
 import Settings from "./Settings";
